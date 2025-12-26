5964b0df5b19e933ea265937a7de870b3794ed23
Rafael Willwohl Salgado <rafael.salgado@weareplanet.com>
2025-12-26 18:06:34 +0000

    fix path and join room with url

---
+++ src/RWS.PlanningPoker/Components/StateContainer.cs
@@ -0,0 +1,20 @@
+namespace RWS.PlanningPoker.Components;
+
+public class StateContainer
+{
+    private string? savedString;
+
+    public string Property
+    {
+        get => savedString ?? string.Empty;
+        set
+        {
+            savedString = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public event Action? OnChange;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
